3d5d91c44bd4c4fc32065012d88ef8028f60e6bf
Marcelo <mabc@live.cl>
2022-03-14 19:27:14 -0300

    Primer commit del proyecto

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+import router from './router'
